c53f27acdee964e6e6f3133886ce35ca6017e8a7
Christina Long <cvlong@gmail.com>
2017-02-25 12:15:51 -0800

    Remove comments

--- index.js
+++ index.js
@@ -11,21 +11,10 @@ app.on('ready', () => {
 	mainWindow.loadURL(`file:///${index}`);
 	// loadURL('file:///Users/christinalong/.../index.html')
 
-	// make sure this window does not navigate
-	// webContents are this cool thing that a browser window in chrome can have
-	// intercepting a navigation attempt is pretty much impossible
-	// but can use this object to get elements from chrome developer
 	mainWindow.webContents.on('will-navigate', (e, url) => {
 		e.preventDefault();
 		console.log(`Tried to navigate to ${url}`);
 
-	// but the browser window doesn't know that this happened
-	// Tried to navigate to file:///Users/ChristinaLong/Desktop/sfbikemap.pdf
-
-	// IPC allows us to send events/objects between the main process 
-	// two components: IPC main & IPC processer
-	// update renderer.js so that navigate on event, and console.log(e)
-	// sends a navigate event to the main window, which is listening
 		mainWindow.webContents.send('navigate', url);
 	});
 });
