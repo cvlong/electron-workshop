225f0f8295302a0498363fab47d922d49af8aa63
Christina Long <cvlong@gmail.com>
2017-02-25 12:13:20 -0800

    Recognize file on drop

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-
-// npm install --save electron
-// npm run
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
 
 let mainWindow;
 
@@ -10,7 +8,24 @@ app.on('ready', () => {
 	const index = path.join(__dirname, 'index.html');
 
 	mainWindow = new BrowserWindow({});
-	// want a protocol
 	mainWindow.loadURL(`file:///${index}`);
 	// loadURL('file:///Users/christinalong/.../index.html')
+
+	// make sure this window does not navigate
+	// webContents are this cool thing that a browser window in chrome can have
+	// intercepting a navigation attempt is pretty much impossible
+	// but can use this object to get elements from chrome developer
+	mainWindow.webContents.on('will-navigate', (e, url) => {
+		e.preventDefault();
+		console.log(`Tried to navigate to ${url}`);
+
+	// but the browser window doesn't know that this happened
+	// Tried to navigate to file:///Users/ChristinaLong/Desktop/sfbikemap.pdf
+
+	// IPC allows us to send events/objects between the main process 
+	// two components: IPC main & IPC processer
+	// update renderer.js so that navigate on event, and console.log(e)
+	// sends a navigate event to the main window, which is listening
+		mainWindow.webContents.send('navigate', url);
+	});
 });
--- renderer.js
+++ renderer.js
@@ -1,6 +1,8 @@
 const loader = require('monaco-loader')
+const {ipcRenderer} = require('electron')
+const fs = require('fs')
 
-console.log('hi')
+console.log('hi');
 
 window.onload = () => {
 	loader().then(monaco => {
@@ -8,6 +10,19 @@ window.onload = () => {
 	    language: 'javascript',
 	    theme: 'vs-dark',
 	    automaticLayout: true
+	  })
+
+	  ipcRenderer.on('navigate', (e, url) => {
+	  	url = url.slice(7);
+	  	console.log(`Trying to read file from ${url}`);
+
+	  	fs.readFile(url, 'utf8', (error, result) => {
+	  		if (!error) {
+	  			console.log(result);
+
+	  			editor.setModel(monaco.editor.createModel(result, 'javascript'));
+	  		}
+	  	});
 	  })
 	})
 }
